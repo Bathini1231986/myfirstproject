050659bf36cbfcfe762ee9a90523dbaa8015b157
anitha <anitha@192.168.0.6>
2018-04-25 11:42:06 +0530

    Test Commit

---
+++ WelcomePage/src/sample/GooglePage.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GooglePage {
+	
+	public WebDriver driver;
+	
+	
+	@Test
+	public void setup()
+	{
+
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\anitha\\Desktop\\testproject\\WelcomePage\\projectjars\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("https://www.google.com");
+		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+	
+}
